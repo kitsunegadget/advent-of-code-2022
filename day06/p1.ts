d7d3e3cd51a12da2009e87588c0e96648f4f8ccb
Yu Kitsune <kitsunegadget@gmail.com>
2023-06-21 01:11:33 +0900

    Add day06

---
+++ day06/p1.ts
@@ -0,0 +1,33 @@
+import { readFromFile } from "../utils";
+
+const tuningTrouble = (line: string) => {
+  const received: string[] = [];
+
+  for (let i = 0; i < line.length; i++) {
+    const char = line[i];
+    const lastAppearedIndex = received.lastIndexOf(char);
+
+    if (lastAppearedIndex !== -1) {
+      received.splice(0, lastAppearedIndex + 1);
+    }
+
+    received.push(char);
+
+    if (received.length === 4) {
+      return i + 1;
+    }
+  }
+};
+
+(async () => {
+  const examples = await readFromFile("./example");
+  const [exampleA, exampleB, exampleC, exampleD, exampleE] = examples;
+  console.log(tuningTrouble(exampleA)); // 7
+  console.log(tuningTrouble(exampleB)); // 5
+  console.log(tuningTrouble(exampleC)); // 6
+  console.log(tuningTrouble(exampleD)); // 10
+  console.log(tuningTrouble(exampleE)); // 11
+
+  const [input] = await readFromFile("./input");
+  console.log(tuningTrouble(input)); // 1779
+})();
---
+++ day06/p2.ts
@@ -0,0 +1,33 @@
+import { readFromFile } from "../utils";
+
+const tuningTrouble = (line: string) => {
+  const received: string[] = [];
+
+  for (let i = 0; i < line.length; i++) {
+    const char = line[i];
+    const lastAppearedIndex = received.lastIndexOf(char);
+
+    if (lastAppearedIndex !== -1) {
+      received.splice(0, lastAppearedIndex + 1);
+    }
+
+    received.push(char);
+
+    if (received.length === 14) {
+      return i + 1;
+    }
+  }
+};
+
+(async () => {
+  const examples = await readFromFile("./example");
+  const [exampleA, exampleB, exampleC, exampleD, exampleE] = examples;
+  console.log(tuningTrouble(exampleA)); // 19
+  console.log(tuningTrouble(exampleB)); // 23
+  console.log(tuningTrouble(exampleC)); // 23
+  console.log(tuningTrouble(exampleD)); // 29
+  console.log(tuningTrouble(exampleE)); // 26
+
+  const [input] = await readFromFile("./input");
+  console.log(tuningTrouble(input)); // 2635
+})();
